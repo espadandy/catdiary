58a9236367c3cc4da233d463ed6fc4466dda75b4
yxu-onshape <yxu@ptc.com>
2024-10-04 00:59:13 -0400

    Initial Commit

---
+++ catdairy/catdairyApp.swift
@@ -0,0 +1,17 @@
+//
+//  catdairyApp.swift
+//  catdairy
+//
+//  Created by Andy Xu on 2024-10-04.
+//
+
+import SwiftUI
+
+@main
+struct catdairyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
